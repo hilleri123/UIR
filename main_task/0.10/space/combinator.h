07e130b33718726bdc50ff8e9fa11cb0ad519a1d
shurik <shurik@skotch.nirvana>
2019-03-21 17:36:30 +0300

    0.08macro

--- main_task/0.10/space/combinator.h
+++ main_task/0.10/space/combinator.h
@@ -15,11 +15,12 @@ std::vector<PartOfFunction<scalar, t>>& combine(const std::vector<std::pair<Poin
 	using PF = PartOfFunction<scalar, t>;
 	//auto& result = *new std::vector<PartOfFunction<scalar, t>>();
 	auto& result = *new std::vector<PF>();
+	PF* part;
 	for (auto i = points.begin(); i+1 < points.end(); i++) {
 		//std::cout << "get<0> = " << std::get<0>(*i) << " get<1> = " << std::get<0>(*(i+1)) << std::endl;
 		//result.emplace_back(std::get<0>(*i), std::get<0>(*(i+1)));
-		INIT(part, (PartOfFunction<scalar, t>), std::get<0>(*i), std::get<0>(*(i+1)) )
-		// INIT (part, PF, std::get<0>(*i), std::get<0>(*(i+1)) )
+		//INIT(part, (PartOfFunction<scalar, t>), std::get<0>(*i), std::get<0>(*(i+1)) );
+		INIT (part, PF, std::get<0>(*i), std::get<0>(*(i+1)) );
 		result.push_back(*part);
 	}
 	return result;
--- main_task/0.10/space/init.h
+++ main_task/0.10/space/init.h
@@ -2,12 +2,18 @@
 #pragma once
 
 
-#define INIT(result, init_class, ...) \
-	auto* result = new init_class(__VA_ARGS__);\
-	if ( ! result->init() ) {\
-		delete result;\
-		result = nullptr;\
-	}\
+#define INIT_START(result, init_class, ...) \
+do { \
+	auto* __macro_tmp = new init_class(__VA_ARGS__);\
+	if ( __macro_tmp->init() ) \
+		result = __macro_tmp; \
+	else {\
+		delete __macro_tmp;\
+		result = nullptr;
+#define INIT_END \
+	} \
+} while (0)
+#define INIT(result, init_class, ...) INIT_START(result, init_class, __VA_ARGS__) INIT_END
 
 
 class base_init
