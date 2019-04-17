7c59d2e93066c7c1a6d12f6f3460590df4ce0724
shurik <shurik@skotch.nirvana>
2019-04-17 19:43:17 +0300

    0.11

--- main_task/program/main.cpp
+++ main_task/program/main.cpp
@@ -1,21 +1,22 @@
 
 #include <iostream>
 
+//#include "function.h"
+//#include "csv_parser.h"
+//#include "space/functions/velocity.h"
 #include "function.h"
 #include "csv_parser.h"
-#include "space/functions/velocity.h"
+#include "velocity.h"
 
-using MyFunction = Function<double, double, double>;
 
 int main(int argc, char** argv)
 {
 	if (argc != 2) {
 		return 1;
 	}
-	//auto data = csv_parser_read<double, double>("test_data.csv");
-	auto& data = csv_parser_read<double, double>(argv[1]);
-	MyFunction a(data);
-	for (double time = 0; time < 10; time+=0.1) {
+	auto& data = csv_parser_read(argv[1]);
+	Function a(data);
+	for (double time = 0; time < 40; time+=0.1) {
 		std::cout << time << " " << a(time).x() << " " << a(time).y() << " " << a(time).z() << std::endl;
 	}
 	delete &data;
