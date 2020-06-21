f031cf14e0bad13aa0a395ac53bf08cf48f58393
shurik <shurik@skotch.nirvana>
2020-06-21 12:33:05 +0300

    course

--- main_task/program/space/functions/sources/combinator.cpp
+++ main_task/program/space/functions/sources/combinator.cpp
@@ -16,6 +16,11 @@ std::vector<PartOfFunction>* combine(const std::vector<std::pair<Point, Velocity
 		std::string tmp = " H("+std::to_string(earth::H(point))+") lat("+std::to_string(point.latitude()*45/atan(1))+") long("+std::to_string(point.longitude()*45/atan(1))+")";
 		my_log::log_it(my_log::level::info, __FUNCTION_NAME__, point.to_string() + " v(" + std::to_string(std::get<1>(*i).v()) + ") "+tmp);
 	}
+	
+	Point fp = std::get<0>(*points.begin());
+	Point sp = std::get<0>(*(points.begin()+1));
+	double lat1 = fp.latitude(), lat2 = sp.latitude(), L = sp.longitude() - fp.longitude(), s = 0, z1 = 0, z2 = 0;
+	inverse(lat1, lat2, L, s, z1, z2);
 
 	//auto direction = Vector(std::get<0>(*points.begin()), std::get<0>(*(points.begin()+1)));
 	//auto&& fv =  Vector(Point(), std::get<0>(*points.begin()));
@@ -26,9 +31,8 @@ std::vector<PartOfFunction>* combine(const std::vector<std::pair<Point, Velocity
 	Vector&& fs_ort = fv * sv;
 	//std::cout << "pre dir" << fv << sv << fs_ort << std::endl;
 	my_log::log_it(my_log::level::debug, __FUNCTION_NAME__, "pre dir " + fv.to_string() + sv.to_string() + fs_ort.to_string());
-	Vector direction = fs_ort * fv;
-	//auto direction = fv.rotate(fs, 2*atan(1));
-	//std::cout << "direction " << direction << std::endl;
+	//Vector direction = fs_ort * fv;
+	Vector direction = earth::course_to_vec(fp, z1);
 
 	for (auto i = points.begin(); i+1 < points.end(); i++) {
 		//std::cout << "direction " << direction << std::endl;
